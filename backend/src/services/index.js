4d95cbe9721a1e95df1d00197a10ac7023bf30fe
sky <gokturk.acr2002@gmail.com>
2023-01-20 00:39:21 +0300

    exports all services

---
+++ backend/src/services/index.js
@@ -0,0 +1,5 @@
+const boosterService = require('./booster-service')
+const customerService = require('./customer-service')
+const orderService = require('./order-service')
+
+module.exports = { boosterService, customerService, orderService }
