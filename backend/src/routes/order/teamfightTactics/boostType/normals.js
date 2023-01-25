39f2e6358ac636579604da8f6b150c8450d25f1e
sky <gokturk.acr2002@gmail.com>
2023-01-26 01:09:24 +0300

    normals boost

---
+++ backend/src/routes/order/teamfightTactics/boostType/normals.js
@@ -0,0 +1,26 @@
+const orderService = require('../../../../services/order-service')
+
+const router = require('express').Router()
+
+router.post('/', async (req, res, next) => {
+    const { customer, server, gamesAmount, options, extraOptions } = req.body
+
+    try {
+        const teamfightTacticsOrder = await orderService.insert({
+            customer: customer,
+            state: 'active',
+            gameType: 'Teamfight Tactics',
+            server: server,
+            orderType: 'normals',
+            gamesAmount: gamesAmount,
+            options: options,
+            extraOptions: extraOptions
+        })
+
+        res.send(teamfightTacticsOrder)
+    } catch (e) {
+        next(e)
+    }
+})
+
+module.exports = router
