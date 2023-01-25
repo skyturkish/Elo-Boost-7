af0bc74b620426d2d2e8d93e82aa7e2add7076fd
sky <gokturk.acr2002@gmail.com>
2023-01-26 00:55:16 +0300

    add Compound Indexes this model is base order Schema, After that, all future features will be written specifically for that order, and the scheme will be expand

--- backend/src/models/order.js
+++ backend/src/models/order.js
@@ -1,24 +1,13 @@
 const mongoose = require('mongoose')
-const Rank = require('./rank')
 const autopopulate = require('mongoose-autopopulate')
-
 const OrderSchema = new mongoose.Schema(
     {
-        currentRank: {
-            type: Rank,
-            required: true
-        },
-        destinationRank: {
-            type: Rank,
-            required: true
-        },
         customer: {
             type: mongoose.ObjectId,
             ref: 'Customer',
             autopopulate: false,
             required: true
         },
-
         booster: {
             type: mongoose.ObjectId,
             ref: 'Booster',
@@ -26,48 +15,29 @@ const OrderSchema = new mongoose.Schema(
         },
         state: {
             type: String,
-            // change name of free
-            enum: ['free', 'taken', 'released', 'canceled', 'completed'],
-            default: 'free'
-        },
-        server: {
-            type: String,
             enum: [
-                'Brazil',
-                'Europe Nordic & East',
-                'Europe West',
-                'Latin America North',
-                'Latin America South',
-                'North America',
-                'Oceania',
-                'Russia',
-                'Turkey',
-                'Japan',
-                'Republic of Korea',
-                'The Philippines',
-                'Singapore, Malaysia, & Indonesia',
-                'Taiwan, Hong Kong, and Macao',
-                'Thailand',
-                'Vietnam'
+                'active',
+                'process',
+                'released',
+                'unavailable',
+                'canceled',
+                'completed'
             ],
+            default: 'active'
+        },
+        gameType: {
+            type: String,
             required: true
         }
     },
     {
-        timestamps: true,
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
+        strict: false,
+        timestamps: true
     }
 )
 
-// not stored ? this is what we want ???
-// we will query with price ??? not sort, query
-OrderSchema.virtual('price').get(function () {
-    // destinationRank - currentRank
-    // calculate price with the formule ????
-    return 100
-})
-
 OrderSchema.plugin(autopopulate)
+
+OrderSchema.index({ gameType: 1, state: 1, booster: 1 })
 
 module.exports = mongoose.model('Order', OrderSchema)
