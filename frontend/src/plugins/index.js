201bce33d680bef51132bccac8dedcf0fac379b4
sky <gokturk.acr2002@gmail.com>
2023-01-26 14:57:27 +0300

    prettier

--- frontend/src/plugins/index.js
+++ frontend/src/plugins/index.js
@@ -10,10 +10,7 @@ import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 
-export function registerPlugins (app) {
-  loadFonts()
-  app
-    .use(vuetify)
-    .use(router)
-    .use(pinia)
+export function registerPlugins(app) {
+    loadFonts()
+    app.use(vuetify).use(router).use(pinia)
 }
