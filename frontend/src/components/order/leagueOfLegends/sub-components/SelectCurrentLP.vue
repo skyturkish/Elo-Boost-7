2efb85a4dd1546efe6239b1c12a26d8dea17153a
sky <gokturk.acr2002@gmail.com>
2023-02-06 02:13:38 +0300

    Select Menu, CurrentLP stored in currentOrder items = ['0-20LP', '20-40LP', '40-60LP', '60-80LP', '80-100LP']

---
+++ frontend/src/components/order/leagueOfLegends/sub-components/SelectCurrentLP.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCurrentOrder } from '@/store/currentOrder'
+
+const CurrentOrderStore = useCurrentOrder()
+
+</script>
+
+<template>
+    <v-select
+    v-model="CurrentOrderStore.currentLP"
+    label="Current LP"
+    :items="['0-20LP', '20-40LP', '40-60LP', '60-80LP', '80-100LP']"
+    ></v-select>
+</template>
