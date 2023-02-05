0cbefd3fcf31ec2772b7ac901bae84da1c3315a9
sky <gokturk.acr2002@gmail.com>
2023-02-06 02:11:53 +0300

    division mile stone [I,II,III,IV] stored in currentOrder

---
+++ frontend/src/components/order/leagueOfLegends/sub-components/DivisionMileStones.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+import { useCurrentOrder } from '@/store/currentOrder'
+
+const CurrentOrderStore = useCurrentOrder()
+
+const divisionMileStones= [ 'I', 'II' ,'III', 'IV']
+
+</script>
+
+<template>
+    <div class="divisionMileStones">
+        <v-btn 
+        v-for="milestone in divisionMileStones" 
+        :key="milestone" 
+        variant="tonal" 
+        @click="CurrentOrderStore.changeMileStone(milestone)">{{ milestone }}</v-btn>
+    </div>
+</template>
+
+<style scoped>
+.divisionMileStones {
+  display: flex;
+  gap: 1rem;
+}
+</style>
+
