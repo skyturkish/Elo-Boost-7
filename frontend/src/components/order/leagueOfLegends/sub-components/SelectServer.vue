88d1634eab9806a4be81e8cd3cf1e3c86dcb0dca
sky <gokturk.acr2002@gmail.com>
2023-02-06 02:16:51 +0300

    Create SelectMenu to select server

---
+++ frontend/src/components/order/leagueOfLegends/sub-components/SelectServer.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useCurrentOrder } from '@/store/currentOrder'
+
+const CurrentOrderStore = useCurrentOrder()
+
+</script>
+
+<template>
+    <v-select
+    v-model="CurrentOrderStore.server"
+    label="Select Server"
+    :items="['Turkey','Vietnam','Eu West']"
+    ></v-select>
+</template>
