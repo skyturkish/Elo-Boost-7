175bdbe32ba012c2694d38abf54b3f9279ff3597
sky <gokturk.acr2002@gmail.com>
2023-02-06 01:56:43 +0300

    add and use cors to fix Access to XMLHttpRequest error => https://stackoverflow.com/questions/57009371/access-to-xmlhttprequest-at-from-origin-localhost3000-has-been-blocked the answers worked in the solution and the problem is solve

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const boosterRouter = require('./src/routes/booster')
 const customerRouter = require('./src/routes/customer')
 const orderRouter = require('./src/routes/order/order')
 
+var cors = require('cors')
+
 // body-parser helps to understand this file send with post is JSON
 const bodyParser = require('body-parser')
 
@@ -12,6 +14,8 @@ require('./src/routes/mongo-connection')
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(cors())
 
 app.use('/', indexRouter)
 app.use('/booster', boosterRouter)
