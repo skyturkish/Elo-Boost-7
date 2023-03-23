77213f40feda3e99f45cb676b42c675b1574eab1
skyturkish <gokturk.acr2002@gmail.com>
2023-03-23 20:36:29 +0300

    can work with docker container and without docker container can take dynamic string from Dockerfile

--- backend/src/routes/mongo-connection.js
+++ backend/src/routes/mongo-connection.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect('mongodb://0.0.0.0:27017/game_elo_boost')
+const connectionString =
+    process.env.MONGODB_CONNECTION_STRING ||
+    'mongodb://0.0.0.0:27017/game_elo_boost'
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 var db = mongoose.connection
 
@@ -11,3 +18,5 @@ db.on('error', console.error.bind(console, 'connection error'))
 db.once('open', function () {
     console.log('we are connected to mongodb!')
 })
+
+module.exports = { mongoose }
