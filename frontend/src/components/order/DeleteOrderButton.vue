5b5a9d5faf5113832c07dfe023e375feb22b9fc5
skyturkish <gokturk.acr2002@gmail.com>
2023-03-23 23:00:57 +0300

    delete order button

---
+++ frontend/src/components/order/DeleteOrderButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { toRefs } from 'vue'
+import axios from 'axios'
+
+const props = defineProps({
+  orderId: {
+    type: String,
+    required: true
+  }
+})
+const { orderId } = toRefs(props);
+
+async function deleteOrder() {
+  const message = await axios.delete(`http://localhost:3000/order/${orderId.value}`)
+  return message
+}
+
+</script>
+<template lang="pug">
+v-btn(variant='outlined' color='error' @click='deleteOrder') Delete
+</template>
