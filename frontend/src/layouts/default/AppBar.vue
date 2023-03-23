c75104579a732000f7b44e05ad0d10f5ab95668a
skyturkish <gokturk.acr2002@gmail.com>
2023-03-23 22:40:38 +0300

    routers

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -2,12 +2,23 @@
   <v-app-bar flat>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      D891U Boost
     </v-app-bar-title>
+    <template v-slot:append>
+    <div class="links">
+      <router-link to="/">Main </router-link>
+      <router-link to="/orders">Orders</router-link>
+      <router-link to="/boosting">Boosting </router-link>
+      <router-link to="/customers">Customers </router-link>
+      <router-link to="/boosters">Boosters </router-link>
+    </div>
+  </template>
   </v-app-bar>
 </template>
 
-<script setup>
-  //
-</script>
+<style scoped>
+.links {
+  display: flex;
+  gap: 1.5rem;
+}
+</style>
