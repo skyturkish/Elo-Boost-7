1d50d8c7cc431731f2f10ed82acf853808774489
skyturkish <gokturk.acr2002@gmail.com>
2023-03-23 22:05:24 +0300

    set up cors options for cookies

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,13 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.use(cors())
+const corsOptions = {
+    origin: 'http://localhost:5000',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 app.use('/', indexRouter)
 app.use('/booster', boosterRouter)
