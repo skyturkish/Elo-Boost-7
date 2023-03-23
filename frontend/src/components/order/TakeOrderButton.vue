c84fa9785d4eb6cf1cd4951cfbdc72c9d9c00254
skyturkish <gokturk.acr2002@gmail.com>
2023-03-23 23:01:36 +0300

    take order for a booster button

---
+++ frontend/src/components/order/TakeOrderButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { toRefs } from 'vue'
+import axios from 'axios'
+
+const props = defineProps({
+  orderId: {
+    type: String,
+    required: true
+  }
+})
+const { orderId } = toRefs(props);
+
+async function takeOrder(){
+  await axios.patch('http://localhost:3000/order/leagueOfLegends', {
+    orderId: orderId.value,
+    object: {
+      state: 'taken',
+      booster: '63f3e19c11ac9dd322e95d45',
+    }
+  })
+}
+
+</script>
+
+<template lang="pug">
+v-btn(variant='outlined' @click='takeOrder')
+  | Take Order
+</template>
