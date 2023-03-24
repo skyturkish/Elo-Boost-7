00861196369a4e0ed79b153545da6408573c519c
skyturkish <gokturk.acr2002@gmail.com>
2023-03-24 17:55:11 +0300

    cors options frontend url

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(bodyParser.json())
 
 const corsOptions = {
-    origin: 'http://localhost:8080',
+    origin: 'https://game-elo-boost-luvjvoipkq-uc.a.run.app/',
     credentials: true, //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
