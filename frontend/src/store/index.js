351d0dddbf0ed14de4598a1212622764ef7f183e
skyturkish <gokturk.acr2002@gmail.com>
2023-03-30 14:31:09 +0300

    set baseurl

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,8 @@
 // Utilities
 import { createPinia } from 'pinia'
+
 import axios from 'axios'
 
-// axios.defaults.baseURL =
-//     'https://game-elo-boost-backend-luvjvoipkq-uc.a.run.app'
 axios.defaults.baseURL = process.env.baseURL || 'http://localhost:3000'
 
 axios.defaults.withCredentials = true
