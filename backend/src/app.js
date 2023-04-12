426ddb8fec469cdc01349919408fb4ad8ba4cddc
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-04-12 13:26:45 +0300

    Handle errors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -50,16 +50,21 @@ app.use('/user', userRouter)
 app.use('/order', orderRouter)
 app.use('/account', accountRouter)
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404))
+})
+
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message
-    res.locals.error = err
-
-    console.log(err)
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
     res.status(err.status || 500)
-    res.send({ message: err.message })
+    res.send(
+        req.app.get('env') === 'development' ? { stack: err.stack, message: err.message } : { message: err.message }
+    )
 })
 
 module.exports = app
