679e56ac3e90cc14f4f40f23c857f98a16ad5068
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-05-16 04:28:47 +0300

    a new banner use on main

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,25 +1,33 @@
 <script setup>
-import AppBar from '@/components/main/AppBar'
+import Banner from '@/components/Banner'
+
 import Games from '@/components/main/Games'
 import OurFeatures from '@/components/main/OurFeatures'
 import Boosting from '@/components/main/Boosting'
 import Coaching from '@/components/main/Coaching'
 import Shop from '@/components/main/Shop'
 import Hub from '@/components/main/Hub'
+
+import PurchaseEloBoostingButton from '@/components/main/appbar-components/PurchaseEloBoostingButton'
+import LearnMoreButton from '@/components/main/appbar-components/LearnMoreButton'
 </script>
 
 <template lang="pug">
-.body
-    AppBar()
-    Games()
-    OurFeatures()
-    Boosting()
-    Coaching()
-    Shop()
-    Hub()
-
+Banner(splashArtPath = 'main' title = 'WE ARE THE ONES MAKING IT REAL' subtitle = 'We provide you everything that you need all in one place')
+  .buttons
+    PurchaseEloBoostingButton()
+    LearnMoreButton()
+Games()
+OurFeatures()
+Boosting()
+Coaching()
+Shop()
+Hub()
 </template>
 
 <style scoped>
-
+.buttons {
+  display: flex;
+  gap: 1rem;;
+}
 </style>
