ba779af03aa16fca1b19fc01d534959b7fee68a9
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-05-16 04:26:07 +0300

    create appbar and dynamic banner to use on all view

---
+++ frontend/src/components/Appbar.vue
@@ -0,0 +1,53 @@
+<script setup>
+import Logo from '@/components/main/appbar-components/Logo'
+import ClientAreaButton from '@/components/ClientAreaButton'
+import SettingsButton from '@/components/main/appbar-components/SettingsButton'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template lang="pug">
+.appbar
+  Logo
+  .navigation-and-buttons
+    v-btn.navigation-text(variant="text" @click="router.push('/boosting/valorant/division')") BOOSTING
+    v-btn.navigation-text(variant="text" @click="router.push('/coaching')") COACHING
+    v-btn.navigation-text(variant="text" @click="router.push('/marketplace')") MARKETPLACE
+    ClientAreaButton
+    SettingsButton
+</template>
+
+<style scoped>
+.appbar {
+  display: flex;
+  justify-content: space-between;
+  font-family: Inter;
+}
+.navigation-and-buttons {
+  display: flex;
+  gap: 1rem;
+}
+.navigation-text {
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  font-size: 24px;
+  font-weight: bold;
+  line-height: normal;
+  color: #fff;
+}
+@media (max-width: 959px) {
+  .navigation-text {
+    font-size: 16px;
+  }
+}
+@media (min-width: 960px) and (max-width: 1263px) {
+  .navigation-text {
+    font-size: 20px;
+  }
+}
+@media (min-width: 1264px) and (max-width: 1903px) {
+  .navigation-text {
+    font-size: 24px;
+  }
+}
+</style>
---
+++ frontend/src/components/Banner.vue
@@ -0,0 +1,74 @@
+<script setup>
+import AppBar from '@/components/Appbar'
+
+const props = defineProps({
+  splashArtPath: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+    required: true
+  },
+  backgroundColor: {
+    type: String,
+    default: '#101922'
+  }
+})
+
+const imgUrls = import.meta.glob('../assets/banners/*.png', {
+  import: 'default',
+  eager: true
+})
+</script>
+
+<template lang="pug">
+.banner(:style="{ backgroundColor: backgroundColor }")
+  v-img.image(
+    aspect-ratio='16/9'
+    cover=''
+    :src='imgUrls[`../assets/banners/${splashArtPath}.png`]')
+    .content
+      AppBar
+      .TRUST-PILOT-5-STAR-REVIEW ** TRUST PILOT 5 STAR REVIEW **
+      .title {{ title }}
+      .subtitle {{ subtitle }}
+      slot
+</template>
+
+<style scoped>
+.image {
+  max-height: 600px;
+  max-width: 1440px;
+  margin: 0 auto;
+}
+.content {
+  display:flex;
+  flex-direction: column;
+  max-width: 1440px;
+  min-width: 960px;
+  margin: 0 auto;
+  padding: 3.6rem;
+  gap: 1rem;
+}
+.TRUST-PILOT-5-STAR-REVIEW {
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+}
+.title {
+  font-size: 48px;
+  font-weight: bold;
+  color: #fff;
+}
+.subtitle {
+  font-family: Inter;
+  font-size: 32px;
+  font-weight: 500;
+  color: #fff;
+}
+</style>
