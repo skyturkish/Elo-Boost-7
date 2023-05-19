e5e89181b6c14a498d4fa7fb2fc82e8a35f0b8ad
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-05-20 00:55:15 +0300

    coaching games menu

---
+++ frontend/src/components/menus/CoachingGamesMenu.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const props = defineProps({
+  gameName: {
+    type: String,
+    required: true
+  },
+  gameImagePath: {
+    type: String,
+    required: true
+  }
+})
+
+const games = [
+  {
+    url: '/coaching/league-of-legends/lesson',
+    name: 'LEAGUE OF LEGENDS',
+    imagePath: 'league-of-legends',
+  },
+  {
+    url: '/coaching/valorant/lesson',
+    name: 'VALORANT',
+    imagePath: 'valorant',
+  },
+]
+const imgUrls = import.meta.glob('../../assets/icons/*.png', {
+  import: 'default',
+  eager: true
+})
+
+function replacePage(game){
+  router.push({path: game.url})
+}
+</script>
+
+<template lang="pug">
+v-menu(max-width='100px')
+  template(v-slot:activator='{ props }')
+    div.select-games(@click='' v-bind='props')
+      .game-image
+        v-img(:src='imgUrls[`../../assets/icons/${gameImagePath}.png`]')
+      .circle ●
+      .game-name {{ gameName }}
+      v-icon(icon="mdi-menu-down")
+  v-list
+    v-list-item(v-for='game in games' :key='game.name' @click='replacePage(game)')
+      template(v-slot:prepend='')
+        v-img(:src='imgUrls[`../../assets/icons/${game.imagePath}.png`]' width='30' height='30')
+      v-list-item-title(v-text='game.name')
+</template>
+
+<style scoped>
+
+.circle {
+  width: 5px;
+}
+
+.select-games {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  cursor: pointer;
+  width: 334px;
+  height: 70.1px;
+  border-radius: 15px;
+  background-color: #fff;
+}
+.game-image {
+  width: 3rem;
+}
+.game-name {
+  font-family: Inter;
+  font-size: 20px;
+  font-weight: 800;
+}
+.row {
+  display: flex;
+}
+</style>
