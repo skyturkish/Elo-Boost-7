41f591ed2a33fbe80c6d1a3e326561210fb354ea
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-05-20 15:09:40 +0300

    change logo size

--- frontend/src/components/main/appbar-components/Logo.vue
+++ frontend/src/components/main/appbar-components/Logo.vue
@@ -10,8 +10,8 @@ v-btn.logo(
 <style scoped>
 
 .logo {
-    width: 8rem;
-    height: 8rem;
+    width: 9.25rem;
+    height: 9.25rem;
     border-radius: 1.6rem;
     background-color: #d9d9d9;
 }
