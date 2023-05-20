de5b6b506cdd30615166b547f858e76b6bbe2b8d
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-05-20 23:35:51 +0300

    implement basic coaching lesson

--- frontend/src/components/coaching/league-of-legends/Lesson.vue
+++ frontend/src/components/coaching/league-of-legends/Lesson.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { ref } from 'vue'
 import CurrentRank from '@/components/boosting/league-of-legends/CurrentRank'
 import Checkout from '@/components/Checkout'
+
+const hours = ref('2 HOURS')
 
 </script>
 
 <template lang="pug">
 .lesson
-  CurrentRank()
-  Checkout()
+  CurrentRank
+    .hours-of-lesson HOURS OF LESSON
+    v-select(:items="['1 HOURS', '2 HOURS', '3 HOURS', '4 HOURS', '5 HOURS']" v-model="hours")
+  Checkout
 </template>
 
 <style scoped>
@@ -16,5 +21,15 @@ import Checkout from '@/components/Checkout'
   flex-wrap: wrap;
   justify-content: center;
   gap: 25px;
+}
+.hours-of-lesson {
+    font-family: Inter;
+    font-size: 24px;
+    font-weight: bold;
+    letter-spacing: normal;
+    color: #444;
+}
+.v-select {
+  width: 50%;
 }
 </style>
