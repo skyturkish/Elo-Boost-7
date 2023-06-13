cad83a340eb4c067a96678f58125e78223f92cc7
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-06-14 02:37:15 +0300

    Auth menu change

--- frontend/src/components/menus/AuthMenu.vue
+++ frontend/src/components/menus/AuthMenu.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref} from 'vue'
+import { ref } from 'vue'
 import { useAccount } from '@/store/account'
 import { useRouter } from 'vue-router'
 
@@ -7,9 +7,27 @@ const router = useRouter()
 const useAccountStore = useAccount()
 const menu = ref(false)
 
+const links = [
+  {
+    name: 'DASHBOARD',
+    path: '/panel/dashboard'
+  },
+  {
+    name: 'EDIT PROFILE',
+    path: '/edit-profile'
+  },
+  {
+    name: 'TRANSACTIONS',
+    path: '/transactions'
+  },
+  {
+    name: 'CREDENTIALS',
+    path: '/credentials'
+  }
+]
+
 async function logout() {
   await useAccountStore.logout()
-  router.push('/')
 }
 </script>
 
@@ -17,26 +35,35 @@ async function logout() {
 v-menu(v-if="useAccountStore.user" v-model="menu" :close-on-content-click="false")
   template(v-slot:activator='{ props }')
     v-img.profile(v-bind="props" :src="useAccountStore.user.photo")
-  v-card.card(min-width='300')
-    v-list()
-      v-list-item(:prepend-avatar="useAccountStore.user.photo" :title="useAccountStore.user.role" :subtitle="useAccountStore.user.name")
-    h4 Balance: {{ useAccountStore.user.balance }}
-    v-divider()
-    v-btn(prepend-icon="mdi-view-dashboard" variant="outlined" @click="router.push('/panel/dashboard')") Dashboard
-    v-btn(prepend-icon="mdi-logout" variant="outlined" @click="logout()") Logout
+  .menu
+    .link(v-for="link in links" :key="link.name" @click="router.push(link.path)") {{ link.name }}
+    .logout(@click="logout") LOG OUT
 </template>
 
 <style scoped>
+.menu {
+  height: 200px;
+  width: 150px;
+  background-color: #F9F9F9;
+  color: #171120;
+  display: flex;
+  flex-direction: column;
+  border-radius: 8px;
+  font-weight: bold;
+  gap: 20px;
+  padding: 1.5rem 1.7rem;
+  font-size: 12px;
+  line-height: normal;
+}
+
+.logout,
+.link {
+  cursor: pointer;
+}
 .profile {
   width: 50px;
   height: 50px;
   border-radius: 15px;
   cursor: pointer;
-}
-.card{
-  padding: 1rem;
-  display:flex;
-  flex-direction: column;
-  gap: 0.25rem;
 }
 </style>
