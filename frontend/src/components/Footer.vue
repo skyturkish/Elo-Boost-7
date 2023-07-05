35a26a88f52963773a8e1a8f25d4ff273f9b5e54
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-07-05 23:50:44 +0300

    Footer implement

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -33,20 +33,20 @@ const links = {
       name: 'Fresh Accounts',
       path: '/marketplace/fresh-account'
     },
-    {
-      name: 'Buy Premium',
-      path: '/premium'
-    }
   ],
-  LESSONS: [
+  CONTENT: [
     {
-      name: 'Learn to Play',
+      name: 'Coaching Videos',
       path: '/lessons/learn-to-play'
     },
     {
       name: 'OTP Guides',
       path: '/lessons/otp-guides'
-    }
+    },
+    {
+      name: 'Blog',
+      path: '/lessons/otp-guides'
+    },
   ],
   EXTRA: [
     {
@@ -54,13 +54,17 @@ const links = {
       path: '/leaderboard'
     },
     {
-      name: 'Blog',
-      path: '/blog'
+      name: 'Legal',
+      path: '/legal'
     },
     {
-      name: 'Legal',
-      path: '/coaching/league-of-legends/lesson'
-    }
+      name: 'Contact',
+      path: '/contact'
+    },
+    {
+      name: 'FAQ',
+      path: '/faq'
+    },
   ]
 }
 
@@ -68,24 +72,52 @@ const links = {
 
 <template lang="pug">
 .footer-background
-  .footer
-    .title(v-for="(sublinks,title) in links") {{ title }}
-      .sublink(v-for="sublink in sublinks" @click="router.push(sublink.path)") {{sublink.name}}
+  .content
+    .socials
+      img.facebook-social(src="@/assets/icons/facebook.png" alt="facebook")
+      img.discord-social(src="@/assets/icons/discord.png" alt="discord")
+      img.instagram-social(src="@/assets/icons/instagram.png" alt="instagram")
+    .titles
+      .title(v-for="(sublinks,title) in links") {{ title }}
+        .sub-title(v-for="sublink in sublinks" @click="router.push(sublink.path)") {{sublink.name}}
+    .reserved 2023 Divine Gaming Services - All rights reserved.
+    .privacy This website nor the brand isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
 </template>
 
 <style scoped>
+.socials {
+  display: flex;
+  gap: 8px;
+  align-self: self-end;
+}
+.facebook-social,
+.instagram-social {
+  width: 29px;
+  height: 29px;
+  cursor: pointer;
+}
+.discord-social {
+  width: 29px;
+  height: 33px;
+  cursor: pointer;
+}
 .footer-background {
   font-family: Inter;
-  height: 311px;
-  background-color: #000;
+  background-color: #111111;
+  padding-bottom: 100px;
 }
-.footer {
-  display: flex;
-  gap: 2rem;
-  padding:2.5rem;
-  width: 1440px;
+.content {
+  display:flex;
+  flex-direction: column;
+  max-width: 1440px;
   margin: 0 auto;
+  padding-top: 2rem;
+}
+.titles {
+  display: flex;
   justify-content: center;
+  gap: 7rem;
+  padding: 2.5rem;
 }
 .title {
   font-size: 24px;
@@ -95,10 +127,23 @@ const links = {
   flex-direction: column;
   gap: 0.25rem;
 }
-.sublink {
+.sub-title {
   font-size: 16px;
   color: #fff;
   cursor: pointer;
 }
-
+.reserved {
+  font-size: 16px;
+  color: #fff;
+  width: 1160px;
+  margin: 0 auto;
+}
+.privacy {
+  color: #FFF;
+  font-size: 14px;
+  font-weight: 500;
+  width: 1160px;
+  padding-top: 20px;
+  margin: 0 auto;
+}
 </style>
