d36531367f319fcc203511c3d57283619e151e37
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-07-10 20:20:23 +0300

    Add useFindAndModify option to mongodb connection

--- backend/src/routes/mongo-connection.js
+++ backend/src/routes/mongo-connection.js
@@ -6,7 +6,8 @@ const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://0.0
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 
 var db = mongoose.connection
