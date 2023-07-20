76c4ad545fc657b0988c8318dbc4a65da792e8f6
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-07-20 12:44:42 +0300

    remove games unavailable from footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -13,14 +13,14 @@ const links = {
       name: 'Valorant',
       path: '/boosting/valorant/division'
     },
-    {
-      name: 'TFT',
-      path: '/boosting/team-fight-tactics/win'
-    },
-    {
-      name: 'Overwatch',
-      path: '/boosting/overwatch/division'
-    },
+    // {
+    //   name: 'TFT',
+    //   path: '/boosting/team-fight-tactics/win'
+    // },
+    // {
+    //   name: 'Overwatch',
+    //   path: '/boosting/overwatch/division'
+    // },
   ],
   COACHING: [
     {
@@ -31,14 +31,14 @@ const links = {
       name: 'Valorant',
       path: '/coaching/valorant/lesson'
     },
-    {
-      name: 'TFT',
-      path: '/coaching/team-fight-tactics/lesson'
-    },
-    {
-      name: 'Overwatch',
-      path: '/coaching/overwatch/lesson'
-    },
+    // {
+    //   name: 'TFT',
+    //   path: '/coaching/team-fight-tactics/lesson'
+    // },
+    // {
+    //   name: 'Overwatch',
+    //   path: '/coaching/overwatch/lesson'
+    // },
   ],
   MARKETPLACE: [
     {
@@ -92,13 +92,11 @@ const links = {
     .titles
       .title(v-for="(sublinks,title) in links") {{ title }}
         .sub-title(v-for="sublink in sublinks" @click="router.push(sublink.path)") {{sublink.name}}
-    .reserved 2023 Divine Gaming Services - All rights reserved.
     .privacy This website nor the brand isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc. Overwatch, Diablo are trademarks and/or registered trademarks of Blizzard Entertainment Inc.
 </template>
 
 <style scoped>
 .footer-background {
-  margin-top: 3rem;
   background-color: #111111;
   padding-bottom: 100px;
 }
