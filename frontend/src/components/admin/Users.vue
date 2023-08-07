a691514d6b6b59fadf1cc361f75a704018a42896
Gokturk Acar <gokturk.acr2002@gmail.com>
2023-08-07 14:42:34 +0300

    add searchbar by name to find user

--- frontend/src/components/admin/Users.vue
+++ frontend/src/components/admin/Users.vue
@@ -1,10 +1,17 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import axios from 'axios'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 const users = ref(null)
+const searchName = ref('')
+
+const filteredUsers = computed(() => {
+  return users.value.filter(user => {
+    return user.name.toLowerCase().includes(searchName.value.toLowerCase())
+  })
+})
 
 onMounted(() => {
   axios.get('/user').then(res => {
@@ -15,14 +22,17 @@ onMounted(() => {
 
 <template lang="pug">
 .loading(v-if="users == null") Loading
-.users(v-else)
-  .user(v-for="user in users")
-    img.avatar(:src='user.photo' :alt="user.name")
-    .user__name name: {{ user.name }}
-    .user__id id: {{ user._id }}
-    .user__role role: {{ user.role }}
-
-    v-btn(@click="router.push(`/admin/user/${user._id}`)") Edit User
+div(v-else)
+  v-text-field(v-model="searchName" label="Search for users")
+  .users(v-if="users.length > 0")
+    .user(v-if="filteredUsers.length > 0" v-for="user in filteredUsers")
+      img.avatar(:src='user.photo' :alt="user.name")
+      .user__name name: {{ user.name }}
+      .user__id id: {{ user._id }}
+      .user__role role: {{ user.role }}
+      v-btn(@click="router.push(`/admin/user/${user._id}`)") Edit User
+    h1(v-else) There are no users by this name check your search
+  h1(v-else) There is no user in database
 </template>
 
 <style scoped>
@@ -39,6 +49,6 @@ onMounted(() => {
 }
 .user {
   width: 400px;
-  background-color: rgb(103, 95, 84);
+  background-color: rgb(177, 170, 159);
 }
 </style>
